3e5eeb67b2163d86c5346af5ffe90067a7658fd6
Adrian Leong <leongadrian36@gmail.com>
2023-03-04 21:39:51 +0800

    pull top rated movies from tmdb

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
-export default function Home() {
+import React from "react";
+import Movie from "@/app/Movie";
+
+export default async function Home() {
+	const data = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`)
+	const res = await data.json()
+
+	console.log(res)
+
 	return (
 		<main>
 			<h1 className={"text-amber-500"}>Thank you, Next</h1>
+			{res.results.map(movie => (
+					<Movie key={movie.id}
+					       id={movie.id}
+					       title={movie.title}
+					       poster_path={movie.poster_path}
+					       release_date={movie.release_date}
+					/>
+				)
+			)}
 		</main>
 	)
 }
