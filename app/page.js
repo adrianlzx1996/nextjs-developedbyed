8bfa93a7c9db84c2db9c05ba5cf7e64e82de56ab
Adrian Leong <leongadrian36@gmail.com>
2023-03-04 21:21:35 +0800

    use custom font (montserrat)

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<h1>Thank you, Next</h1>
+		<main>
+			<h1 className={"text-amber-500"}>Thank you, Next</h1>
 		</main>
 	)
 }
