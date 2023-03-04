ac3d30809e422deb0328652009b7b161520dd639
Adrian <leongadrian36@gmail.com>
2023-03-04 17:50:39 +0800

    add about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function About() {
 	return (
-		<main className={styles.main}>
-			<h1>Thank you, Next</h1>
+		<main>
+			<h1>About Page</h1>
 		</main>
 	)
 }
